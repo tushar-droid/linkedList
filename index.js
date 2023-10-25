b941020c73b656f730253b5a0b00d40b0d289d1d
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-10-24 23:40:10 -0700

    Added at(index) and pop functions

--- index.js
+++ index.js
@@ -34,6 +34,29 @@ class LinkedList {
 
     head = () => this.head_node.value;
     tail = () => this.tail_node.value;
+
+    at = (index) =>{        
+        if(index>this.size-1 || index<0){
+            return null;
+        }
+        var current_node = this.head_node;
+        for(var i =0; i<index; i++){
+            current_node = current_node.nextNode;
+        }
+        return current_node.value;
+    }
+    pop = () =>{
+        var current_node = this.head_node;
+       
+        while(current_node.nextNode.nextNode != null){
+            current_node = current_node.nextNode;
+        }
+        var val = current_node.nextNode.value;
+        current_node.nextNode = null;
+        this.size = this.size-1;
+        this.tail_node = current_node;
+        return val;
+    }
     
 }
 
@@ -56,3 +79,22 @@ var val = l.toString();
 console.log(val);
 console.log('The head_node value: ', l.head());
 console.log('The Tail value: ', l.tail());
+console.log('The value at index 0 is: ', l.at(-1));
+console.log('The value poped from the list is: ', l.pop());
+var val = l.toString();
+console.log('The updated looks like: ', val);
+l.append(10);
+l.append(15);
+l.append(17);
+l.prepend(8);
+l.append(23);
+l.prepend(-3);
+var val = l.toString();
+console.log('The new updated looks like: ', val);
+console.log('The value poped from the list is: ', l.pop());
+console.log('The value poped from the list is: ', l.pop());
+console.log('The value poped from the list is: ', l.pop());
+console.log('The value poped from the list is: ', l.pop());
+var val = l.toString();
+console.log('The updated looks like: ', val);
+
